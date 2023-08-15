1fcacf1f3655408bba90ab0f35d8f62f3d3c37d5
energy1-914 <dlawldnjs5@daum.net>
2023-08-16 01:14:57 +0900

    Fix: 페이지 제목 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import Provider from "./Provider";
 import "sfac-designkit-react/style.css";
 
 export const metadata: Metadata = {
-  title: "스나이퍼 팩토리",
-  description: "스나이퍼 팩토리 LMS시스템입니다.",
+  title: "LMS 학습관리 시스템",
+  description: "LMS 학습관리 시스템입니다.",
 };
 
 export default function RootLayout({
