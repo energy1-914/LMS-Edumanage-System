1462c27c4b386f2075a9824fb0c40f79b14cd37d
energy1-914 <dlawldnjs5@daum.net>
2023-08-25 00:46:52 +0900

    Feat: 전체 활동 로그 항목별 갯수 표시

--- src/app/mypage/(components)/(dashboard)/UserActivityCounters.tsx
+++ src/app/mypage/(components)/(dashboard)/UserActivityCounters.tsx
@@ -1,10 +1,19 @@
+"use client";
+
+import { useUserActivityData } from "@/hooks/common/useUserActivityData";
+import { useAppSelector } from "@/redux/store";
+
 const UserActivityCounters = () => {
+  const userId = useAppSelector(state => state.userInfo.id);
+  const { filteredAssignments, filteredPosts, comments, progressData } =
+    useUserActivityData(userId);
+
   return (
-    <div className="h-[130px] text-base border-solid border border-gray-200 rounded-[10px] px-3 py-4 mb-3 mt-12 cursor-pointer">
-      <div>총 게시글 : </div>
-      <div>댓글 : </div>
-      <div>제출한 과제 : </div>
-      <div>수강 완료 강의 : </div>
+    <div className="h-[130px] text-base border-solid border border-gray-200 rounded-[10px] px-3 py-4 mb-3 mt-10 cursor-pointer">
+      <div>총 게시글 : {filteredPosts?.length}</div>
+      <div>댓글 : {comments.length}</div>
+      <div>제출한 과제 : {filteredAssignments?.length}</div>
+      <div>수강 완료 강의 : {progressData?.completedLectures}</div>
     </div>
   );
 };
