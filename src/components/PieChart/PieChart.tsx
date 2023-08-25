feb77306ee87dc99fb6389341894058a808e0cee
energy1-914 <dlawldnjs5@daum.net>
2023-08-26 02:04:39 +0900

    Feat: 파이차트 공통 컴포넌트 구현

---
+++ src/components/PieChart/PieChart.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import { PieArcDatum } from "d3";
+import { SVG_WIDTH, SVG_HEIGHT, COLORS } from "@/constants/pie-chart";
+
+type PieChartProps = {
+  compeletedTasks: number;
+  totalTasks: number;
+  title: string;
+  subtitle: string;
+  hoverText: string;
+};
+
+const PieChart = ({
+  compeletedTasks,
+  totalTasks,
+  title,
+  subtitle,
+  hoverText,
+}: PieChartProps) => {
+  const svgRef = useRef(null);
+  const containerRef = useRef(null);
+
+  let percentage = Math.round((compeletedTasks / totalTasks) * 100);
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    const radius = Math.min(SVG_WIDTH, SVG_HEIGHT) / 2;
+
+    const color = d3.scaleOrdinal(COLORS);
+    const pie = d3.pie().sort(null);
+    const arc = d3
+      .arc<PieArcDatum<number>>()
+      .innerRadius(radius * 0.7)
+      .outerRadius(radius);
+
+    const data = [percentage, 100 - percentage].map(d => +d);
+
+    svg.attr("width", SVG_WIDTH).attr("height", SVG_HEIGHT);
+
+    const group = svg
+      .append("g")
+      .attr("transform", `translate(${SVG_WIDTH / 2},${SVG_HEIGHT / 2})`);
+
+    const arcs = group
+      .selectAll(".arc")
+      .data(pie(data))
+      .enter()
+      .append("g")
+      .attr("class", "arc");
+
+    const tooltip = d3
+      .select(containerRef.current)
+      .append("div")
+      .attr("class", "tooltip")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("background-color", "white")
+      .style("padding", "8px")
+      .style("border", "1px solid black")
+      .style("border-radius", "5px")
+      .style("font-size", "0.8rem")
+      .style("color", "#555");
+
+    arcs
+      .append("path")
+      .attr("fill", (d, i) => color(`${i}`))
+      .attr("d", d => arc(d as PieArcDatum<number>) || "")
+      .on("mouseover", function (d, i) {
+        if (i.index === 0) {
+          tooltip.text(`${hoverText}: ${percentage}%`);
+        } else {
+          tooltip.text(`미${hoverText}:  ${100 - percentage}%`);
+        }
+        tooltip
+          .style("border-color", color(`${i.index}`))
+          .style("visibility", "visible");
+      })
+      .on("mousemove", function (event) {
+        tooltip
+          .style("top", event.pageY - 10 + "px")
+          .style("left", event.pageX + 10 + "px");
+      })
+      .on("mouseout", function () {
+        tooltip.style("visibility", "hidden");
+      })
+      .transition()
+      .delay((d, i) => i * 500)
+      .duration(500)
+      .attrTween("d", function (d) {
+        const interp = d3.interpolate(d.startAngle, d.endAngle);
+        return function (t) {
+          d.endAngle = interp(t);
+          return arc(d as PieArcDatum<number>) || "";
+        };
+      });
+
+    return () => {
+      tooltip.remove();
+    };
+  }, [percentage]);
+
+  return (
+    <div>
+      <h3 className="text-lg font-bold">{title}</h3>
+      <div
+        ref={containerRef}
+        className="h-full flex justify-center items-center gap-6 border-solid border border-gray-200 rounded-[10px] px-3 py-4 my-3 cursor-pointer"
+      >
+        <svg ref={svgRef} />
+        <small>
+          {subtitle} ({percentage}%)
+        </small>
+      </div>
+    </div>
+  );
+};
+export default PieChart;
