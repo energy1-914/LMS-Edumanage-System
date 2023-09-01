a737e0440030f11489f4ca36b49d8f75b150f488
energy1-914 <dlawldnjs5@daum.net>
2023-09-02 04:21:52 +0900

    Refactor: UserActivityCounters 리팩토링

--- src/app/mypage/(components)/(dashboard)/UserActivityCounters.tsx
+++ src/app/mypage/(components)/(dashboard)/UserActivityCounters.tsx
@@ -7,13 +7,20 @@ const UserActivityCounters = () => {
   const userId = useAppSelector(state => state.userInfo.id);
   const { filteredAssignments, filteredPosts, comments, progressData } =
     useUserActivityData(userId);
-
+  const items = [
+    { text: "총 게시글", count: filteredPosts?.length },
+    { text: "댓글", count: comments.length },
+    { text: "제출한 과제", count: filteredAssignments?.length },
+    { text: "수강 완료 강의", count: progressData?.completedLectures },
+  ];
   return (
-    <div className="h-[130px] text-base border-solid border border-gray-200 rounded-[10px] px-3 py-4 mb-3 mt-10">
-      <div>총 게시글 : {filteredPosts?.length}</div>
-      <div>댓글 : {comments.length}</div>
-      <div>제출한 과제 : {filteredAssignments?.length}</div>
-      <div>수강 완료 강의 : {progressData?.completedLectures}</div>
+    <div className="w-full flex flex-col justify-center text-base border-solid border border-gray-200 rounded-[10px] p-4 mb-3 mt-10">
+      {items.map(item => (
+        <div className="flex justify-between text-lg">
+          <div>{item.text}:</div>
+          <div>{item.count}</div>
+        </div>
+      ))}
     </div>
   );
 };
